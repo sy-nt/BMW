4ce2dd3ab16ddf94b89497ecbdb5fec3beaaa43b
sy-nt <20110715@student.hcmute.edu.vn>
2023-05-29 02:31:24 +0700

    fix validate user

--- models/User.js
+++ models/User.js
@@ -1,44 +1,44 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      required: true,
-      min: 2,
-      max: 50,
+    {
+        firstName: {
+            type: String,
+            required: true,
+            min: [2, "Must be at least 2 letters"],
+            max: [50, "Must be below 50 letters"],
+        },
+        lastName: {
+            type: String,
+            required: true,
+            min: [2, "Must be at least 2 letters"],
+            max: [50, "Must be below 50 letters"],
+        },
+        email: {
+            type: String,
+            required: true,
+            max: [50, "Must be below 50 letters"],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: [5, "Must be at least 5 letters"],
+        },
+        picturePath: {
+            type: String,
+            default: "",
+        },
+        friends: {
+            type: Array,
+            default: [],
+        },
+        location: String,
+        occupation: String,
+        viewedProfile: Number,
+        impressions: Number,
     },
-    lastName: {
-      type: String,
-      required: true,
-      min: 2,
-      max: 50,
-    },
-    email: {
-      type: String,
-      required: true,
-      max: 50,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      min: 5,
-    },
-    picturePath: {
-      type: String,
-      default: "",
-    },
-    friends: {
-      type: Array,
-      default: [],
-    },
-    location: String,
-    occupation: String,
-    viewedProfile: Number,
-    impressions: Number,
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 const User = mongoose.model("User", UserSchema);
